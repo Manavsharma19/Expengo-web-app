d26af933e55880c74d5ad2f8fae998f89643ece5
Manavsharma19 <rlmanavsharma@gmail.com>
2023-02-28 11:11:00 +0000

    Added sliders component from mui

--- src/pages/Home.page.js
+++ src/pages/Home.page.js
@@ -2,6 +2,10 @@ import { Button } from '@mui/material'
 import { useContext } from 'react';
 import { UserContext } from '../contexts/user.context';
 import Typography from '@material-ui/core/Typography';
+import Slider from '@mui/material/Slider';
+import Box from '@mui/material/Box';
+
+
 
  
 export default function Home() {
@@ -22,7 +26,7 @@ export default function Home() {
  return (
    <>
      
-     <Typography 
+     {/* <Typography 
      variant="h1"
      color='primary'
      align='center'
@@ -37,7 +41,57 @@ export default function Home() {
      >
      <Button variant="contained" onClick={logOut}>Logout</Button>
 
+     </Typography> */}
+
+<Typography 
+     variant="h3"
+     color='secondary'
+     align='center'
+     >
+        Welcome to Expengo
+
      </Typography>
+
+     <Typography 
+     variant='h2'
+     align='center'
+     >
+     <Button variant="contained" onClick={logOut}>Logout</Button>
+
+     </Typography>
+
+     <Box width={500}   >
+      <Typography>Attention</Typography>
+      <Slider 
+        defaultValue={50}
+        aria-valuetelabel="default"
+        valueLabelDisplay="auto" 
+        />
+    </Box>
+     <Box width={500}   >
+      <Typography>Stress</Typography>
+      <Slider 
+        defaultValue={50}
+        aria-valuetelabel="Attention"
+        valueLabelDisplay="auto" 
+        />
+    </Box>
+     <Box width={500}   >
+      <Typography>Sleepiness</Typography>
+      <Slider 
+        defaultValue={50}
+        aria-valuetelabel="Attention"
+        valueLabelDisplay="auto" 
+        />
+    </Box>
+     <Box width={500}   >
+      <Typography>Happy</Typography>
+      <Slider 
+        defaultValue={50}
+        aria-valuetelabel="Attention"
+        valueLabelDisplay="auto" 
+        />
+    </Box>
 
    </>
  )
